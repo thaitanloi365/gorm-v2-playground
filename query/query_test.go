b30fafdb865764e63e51542b968cc39475e3ffab
Thái Tấn Lợi <thaitanloi365@gmail.com>
2021-03-07 23:57:19 +0700

    edit gorm

--- query/query_test.go
+++ query/query_test.go
@@ -23,13 +23,20 @@ type Profile struct {
 	Avatar string
 }
 
+type CreditCard struct {
+	Model
+	UserID uint   `json:"user_id"`
+	Last4  string `json:"last_4"`
+}
+
 // User user
 type User struct {
 	Model
-	Email     string   `json:"email"`
-	Phone     string   `json:"phone"`
-	ProfileID string   `json:"profile_id"`
-	Profile   *Profile `json:"profile"`
+	Email       string        `json:"email"`
+	Phone       string        `json:"phone"`
+	ProfileID   string        `json:"profile_id"`
+	Profile     *Profile      `json:"profile"`
+	CreditCards []*CreditCard `json:"credit_cards"`
 }
 
 func TestQuery(t *testing.T) {
@@ -40,35 +47,39 @@ func TestQuery(t *testing.T) {
 	}
 	db = db.Debug()
 
-	New(db.Raw("SELECT users.id AS id, users.profile_id AS profile_id, profiles.id AS profile_id, profiles.created_at AS profile_created_at, profiles.avatar AS profile_avatar FROM users LEFT JOIN profiles ON profiles.id = users.profile_id")).PaginateFunc(func(db *gorm.DB) (records interface{}, err error) {
+	New(db.Raw("SELECT users.id AS user_id, users.profile_id AS user_profile_id, users.profile_id AS profile_id, profiles.id AS profile_id, profiles.created_at AS profile_created_at, profiles.avatar AS profile_avatar FROM users LEFT JOIN profiles ON profiles.id = users.profile_id")).PaginateFunc(func(db *gorm.DB) (records interface{}, err error) {
 		type UserAlias struct {
-			User    *User    `gorm:"embedded"`
+			User    *User    `gorm:"embedded;embeddedPrefix:user_"`
 			Profile *Profile `gorm:"embedded;embeddedPrefix:profile_"`
 		}
 
 		var users []*User
-		rows, err := db.Rows()
-		if err != nil {
-			return users, err
-		}
+		var alias []*UserAlias
+		db.Model(&User{}).Find(&alias)
 
-		for rows.Next() {
-			var alias UserAlias
-			var err = db.ScanRows(rows, &alias)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
+		printJSON(alias)
+		// rows, err := db.Rows()
+		// if err != nil {
+		// 	return users, err
+		// }
 
-			data, err := json.Marshal(&alias)
-			if err != nil {
-				panic(err)
-			}
+		// for rows.Next() {
+		// 	var alias UserAlias
+		// 	var err = db.ScanRows(rows, &alias)
+		// 	if err != nil {
+		// 		fmt.Println(err)
+		// 		continue
+		// 	}
 
-			fmt.Println("data", string(data))
-		}
+		// 	data, err := json.Marshal(&alias)
+		// 	if err != nil {
+		// 		panic(err)
+		// 	}
 
-		fmt.Println("done")
+		// 	fmt.Println("data", string(data))
+		// }
+
+		// fmt.Println("done")
 		return users, nil
 	})
 
@@ -83,16 +94,38 @@ func mockData() {
 		db.Migrator().DropTable(&Profile{})
 	}
 
+	if db.Migrator().HasTable(&CreditCard{}) {
+		db.Migrator().DropTable(&CreditCard{})
+	}
+
+	db.AutoMigrate(&CreditCard{})
 	db.AutoMigrate(&Profile{})
 	db.AutoMigrate(&User{})
 	var createUsers = []*User{}
 	// Seed database
-	for i := 0; i < 20; i++ {
+	for i := 1; i <= 20; i++ {
 		var user = User{
 			Email: fmt.Sprintf("user_%d@test.com", i),
 			Phone: "+12345678910",
 			Profile: &Profile{
 				Avatar: fmt.Sprintf("avatar_%d", i),
+			},
+			CreditCards: []*CreditCard{
+				{
+					Last4: "1111",
+				},
+				{
+					Last4: "1112",
+				},
+				{
+					Last4: "1113",
+				},
+				{
+					Last4: "1114",
+				},
+				{
+					Last4: "1115",
+				},
 			},
 		}
 		createUsers = append(createUsers, &user)
